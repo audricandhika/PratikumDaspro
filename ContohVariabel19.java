0f8b7312bec9384b498120f4d9ef1fb6492b96ba
Audricc <audricc015@gmail.com>
2023-09-20 11:37:23 +0700

    perubahan 1

---
+++ ContohVariabel19.java
@@ -0,0 +1,16 @@
+public class ContohVariabel19{
+    public static void main(String[] args){
+
+      String salahsatuhobisayaadalah = "Bermain Petak Umpet";
+      boolean isPandai = true;
+      char jeniskelamin = 'L';
+      byte _umursayasekarang = 18;
+      double $ipk = 3.24, tinggi = 1.78;
+      
+      System.out.println(salahsatuhobisayaadalah);
+      System.out.println("apakah pandai? " + isPandai);
+      System.out.println("Jenis kelamin: " + jeniskelamin);
+      System.out.println("IPK: " + $ipk);
+      System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", _umursayasekarang, tinggi));
+    }
+}
