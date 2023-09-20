3cf0d778afe5f86e1eae1d0b57557e0ad100e64a
Audricc <audricc015@gmail.com>
2023-09-20 13:36:23 +0700

    Penambahan 2

---
+++ ContohTipeData19.java
@@ -0,0 +1,18 @@
+public class ContohTipeData19{
+    public static void main(String[] args){
+
+        char golongandarah = 'A';
+        short jumlahpenduduk = 1025;
+        float suhu = 60.50f;
+        double berat = 0.5467812345;
+        long saldo = 150000000;
+        int angkadesimal = 0x10;
+
+        System.out.println("golongan darah\t: " + (byte)golongandarah);
+        System.out.println("jumlah penduduk\t: " + jumlahpenduduk);
+        System.out.println("suhu\t\t: " + suhu);
+        System.out.println("berat\t\t: " + (float)berat);
+        System.out.println("saldo\t\t: " + saldo);
+        System.out.println("angka desimal\t: " + angkadesimal);
+    }
+}
--- ContohVariabel19.java
+++ ContohVariabel19.java
@@ -1,13 +1,15 @@
 public class ContohVariabel19{
     public static void main(String[] args){
 
-      String salahsatuhobisayaadalah = "Bermain Petak Umpet";
+      String hobisaya = "Bersepeda";
+      String Namasaya = "Audric";
       boolean isPandai = true;
       char jeniskelamin = 'L';
       byte _umursayasekarang = 18;
       double $ipk = 3.24, tinggi = 1.78;
       
-      System.out.println(salahsatuhobisayaadalah);
+      System.out.println(hobisaya);
+      System.out.println(Namasaya);
       System.out.println("apakah pandai? " + isPandai);
       System.out.println("Jenis kelamin: " + jeniskelamin);
       System.out.println("IPK: " + $ipk);
