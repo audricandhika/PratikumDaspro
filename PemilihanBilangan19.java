ff47376474c91e966c680f7fc93b4fbfe372d57d
Audricc <audricc015@gmail.com>
2023-10-04 11:26:09 +0700

    perubahan

--- PemilihanBilangan19.java
+++ PemilihanBilangan19.java
@@ -4,13 +4,9 @@ public class PemilihanBilangan19{
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String bilangan;
+        
+        bilangan = (angka % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
+        System.out.println(angka + "adalah" + bilangan);
     }
 }
