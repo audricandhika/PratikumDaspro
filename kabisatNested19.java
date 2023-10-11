daf3d3c5a99479dfadd12216f0245e5e3d277dda
Audricc <audricc015@gmail.com>
2023-10-11 13:13:50 +0700

    06

--- kabisatNested19.java
+++ kabisatNested19.java
@@ -9,8 +9,13 @@ public class kabisatNested19{
     if ((tahun % 4) == 0){
         if ((tahun % 100) != 0)
             System.out.println("Tahun kabisat");
+            if((tahun % 400) ==0)
+            System.out.println("Tahun kabisat");
         else
+            System.out.println("Bukan Tahun Kabisat");
+    }        
+    else
         System.out.println("Bukan Tahun Kabisat");
-    }
+    
     }
 }
