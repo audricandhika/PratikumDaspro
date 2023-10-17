78facb3253cf5d5891f7fc892c0640102c3be916
Audricc <audricc015@gmail.com>
2023-10-17 11:26:19 +0700

    6

--- Bilangan19.java
+++ Bilangan19.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Bilangan19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int bil, bil2, bil3;
+        int bil, bil2, bil3, bilanganTerbesar;
 
         System.out.println("Masukkan Bilangan 1");
         bil = sc.nextInt();
@@ -10,27 +10,10 @@ public class Bilangan19 {
         bil2 = sc.nextInt();
         System.out.println("Masukkan Bilangan 3");
         bil3 = sc.nextInt();
+        
+        bilanganTerbesar = bil>bil2 ? (bil>bil3 ? bil : (bil>bil3 ? bil : bil3)) : (bil>bil3 ? bil2 : (bil2>bil3 ? bil2 : bil3));
 
-        if(bil>bil2){
-            if(bil2>bil3){
-                System.out.println("Bilangan Terbersar: " + bil);
-            }else{
-                if(bil>bil3){
-                    System.out.println("Bilangan Terbesar: " + bil);
-                } else{
-                    System.out.println("Bilangan Terbesar: " + bil3);
-                }
-            }
-        }else{
-            if(bil>bil3){
-                System.out.println("Bilangan Terbesar: " + bil2);
-            }else{
-                if(bil2>bil3){
-                System.out.println("Bilangan Terbesar: " + bil2);
-            } else{
-                System.out.println("Bilangan Terbesar: " + bil3);
-            }
-        }
-    }
-}
+        System.out.println("Bilangan Terbesar: " + bilanganTerbesar);
+        bilanganTerbesar = sc.nextInt();
+       }
 }
