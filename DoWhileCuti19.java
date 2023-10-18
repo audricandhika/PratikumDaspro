6817fe097830131e0133db26531f4c5836ade5b5
Audricc <audricc015@gmail.com>
2023-10-18 15:13:43 +0700

    7

---
+++ DoWhileCuti19.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class DoWhileCuti19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.println("Jatah cuti");
+        jatahCuti = sc.nextInt();
+
+        do{
+            System.out.println("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi= sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.println("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah suci: " + jatahCuti);
+                }else{
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0);
+    }
+}
