ff8503bd7fddbd66847211f12ccd92381780a34b
Audricc <audricc015@gmail.com>
2023-11-01 15:22:07 +0700

    9

---
+++ Pratikum09/LinearSearch19.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class LinearSearch19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int key;
+        int hasil = -1;
+        int elemen;
+        
+        System.out.print("Masukkan jumlah elemen array: ");
+        elemen = sc.nextInt();
+        int[] arrayInt = new int[elemen];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" +(i+0) + " : ");
+            arrayInt[i] = sc.nextInt();
+
+        }
+            System.out.println("Masukkan key yang ingin dicari: ");
+            key = sc.nextInt();
+        
+        for (int j = 0; j < arrayInt.length; j++) {
+            if (arrayInt[j] == key) {
+                hasil = j;
+                System.out.println("Key ada di posisi indeks ke-" + hasil);
+            } 
+        }
+        if (hasil == -1) {
+            System.out.println("Key tidak ditemukan");
+        }
+    }
+}
