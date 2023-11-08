869527799552f6513de5cd5f8dd11c01bf729b1a
Audricc <audricc015@gmail.com>
2023-11-08 13:06:39 +0700

    Array 2 dimensi

--- Pratikum10/Bioskop19.java
+++ Pratikum10/Bioskop19.java
@@ -24,10 +24,8 @@ public class Bioskop19 {
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
 
-        System.out.println("Penonton pada baris ke-3: ");
-
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
     }
 }
