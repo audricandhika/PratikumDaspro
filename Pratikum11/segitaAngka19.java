01529772484964fc1afe4c90508913fe4d968fff
Audricc <audricc015@gmail.com>
2023-11-15 15:18:21 +0700

    perulangan

--- Pratikum11/segitaAngka19.java
+++ Pratikum11/segitaAngka19.java
@@ -5,19 +5,28 @@ import java.util.Scanner;
 public class segitaAngka19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Masukkan angka: ");
-        int n = sc.nextInt();
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                System.out.print(" ");
+        
+        while (true) {
+            System.out.print("Masukkan angka: ");
+            int n = sc.nextInt();
+            
+            if (n>2) {
+                for (int i = 0; i <= n; i++) {
+                    for (int j = 0; j < n - i; j++) {
+                        System.out.print(" ");
+                    }
+                    
+                    for (int j = 0; j < i; j++) {
+                        int angka = j + 1;
+                        System.out.print(angka);
+                    }
+                    System.out.println();
+                }
+            } else{
+                System.out.println("Angka tidak valid");
             }
-            for (int j = 0; j < i; j++) {
-                int angka = j + 1;
-                System.out.print(angka);
-            }
-            System.out.println();
+                    
+            
         }
     }
 }
