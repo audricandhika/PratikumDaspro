76b2417d0cffcc832baadf684210fb82a17bbbf8
Audricc <audricc015@gmail.com>
2023-11-15 11:05:26 +0700

    perulangan

--- Pratikum11/Square19.java
+++ Pratikum11/Square19.java
@@ -2,15 +2,19 @@ package Pratikum11;
 
 import java.util.Scanner;
 
-public class Star19 {
+public class Square19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan jumlah *: ");
         int n = sc.nextInt();
 
-        for (int i = 0; i <= n; i--) {
-            System.out.print("*");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
+        
     }
 }
