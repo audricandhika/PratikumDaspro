e4db9e39e74645c42c2783f8d75fe7ab7631c318
Audricc <audricc015@gmail.com>
2023-11-15 14:41:05 +0700

    perulangan

--- Pratikum11/NestedLoop19.java
+++ Pratikum11/NestedLoop19.java
@@ -6,7 +6,11 @@ public class NestedLoop19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double[][] suhu = new double[2][4];
+        double rata = 0;
+        
+        System.out.print("Masukkan jumlah kota:");
+        int jumlahKota = sc.nextInt();
+        double[][] suhu = new double[jumlahKota][4];
 
         for (int i = 0; i < suhu.length; i++) {
             System.out.println("KOTA ke-" + (i + 1));
@@ -19,11 +23,25 @@ public class NestedLoop19 {
         }
 
         for (int i = 0; i < suhu.length; i++) {
-            System.out.println("Kota ke-" + (i+1) + ": ");
+            System.out.print("KOTA ke-" + (i + 1) + ": ");
+
+            for (double Suhu : suhu[i]) {
+                System.out.print(Suhu + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < suhu.length; i++) {
+            System.out.println("Rata-rata suhu kota ke-" + (i + 1));
+            double total = 0;
 
             for (int j = 0; j < suhu[i].length; j++) {
-                System.out.print(suhu [i][j] + " ");
+                total = total + suhu[i][j];
             }
+
+            rata = (double)total / suhu[i].length;
+            System.out.println(rata);
+
             System.out.println();
         }
     }
