0f9a1cc8be692b3215bf5e46f7594b4b5a1c82bc
Audricc <audricc015@gmail.com>
2023-11-29 18:46:42 +0700

    Function

--- Pratikum13/LocalGlobal19.java
+++ Pratikum13/LocalGlobal19.java
@@ -1,10 +1,11 @@
 package Pratikum13;
 
 public class LocalGlobal19 {
-    static int a = 19;
+    static int a = 10;
     public static void main(String[] args) {
         int b = 10;
         int c = 17;
+        a = a + 8;
 
         System.out.println("Nilai a: " + a);
         System.out.println("Nilai b: " + b);
@@ -16,6 +17,7 @@ public class LocalGlobal19 {
     private static void display(){
         int b = 20;
         int c = 5;
+        a = a + 12;
 
         System.out.println("Nilai a: " + a);
         System.out.println("Nilai b: " + b);
