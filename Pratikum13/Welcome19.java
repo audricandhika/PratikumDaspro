548f3b6eaa72a062ca4719b10e5354a8d945baed
Audricc <audricc015@gmail.com>
2023-11-29 11:33:04 +0700

    Function

--- Pratikum13/Welcome19.java
+++ Pratikum13/Welcome19.java
@@ -2,9 +2,10 @@ package Pratikum13;
 
 public class Welcome19 {
     public static void main(String[] args) {
-        displayWelcomeMessage();
-    }
-    public static void displayWelcomeMessage(){
         System.out.println("Selamat datang di politeknik Negeri Malang");
     }
+        
+    //displayWelcomeMessage();
+    //public static void displayWelcomeMessage(){
+    //}
 }
---
+++ Pratikum13/Welcome219.java
@@ -0,0 +1,15 @@
+package Pratikum13;
+
+public class Welcome219 {
+    public static void main(String[] args) {
+        displayWelcomeMessage("Ani");
+        displayWelcomeMessage("BUdi");
+        displayWelcomeMessage("Cica");
+        displayWelcomeMessage("Dodi");
+
+    }
+
+    public static void displayWelcomeMessage(String name){
+        System.out.println("Selamat datang di politeknik Negeri Malang " + name);
+    }
+}
