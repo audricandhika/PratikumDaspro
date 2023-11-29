9221ef45343b10cf775b8a3a6ff1c4622ab6dfbc
Audricc <audricc015@gmail.com>
2023-11-29 14:51:26 +0700

    Fungsi

---
+++ Pratikum13/CobaArgs19.java
@@ -0,0 +1,13 @@
+package Pratikum13;
+
+public class CobaArgs19 {
+    public static void main(String[] args) {
+
+        if (1 < args.length) {
+            System.out.println("Hai " + args[0]);
+            System.out.println("Selamat datang di " + args[1]);
+        } else {
+            System.out.println("Hai, Selamat datang ");
+        }
+    }
+}
