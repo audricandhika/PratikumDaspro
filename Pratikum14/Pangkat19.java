cc6bc56b3e951ee188284e9ebe78b3f2d8600461
Audricc <audricc015@gmail.com>
2023-12-07 19:44:02 +0700

    fungsi 2

---
+++ Pratikum14/Pangkat19.java
@@ -0,0 +1,24 @@
+public class Pangkat19 {
+    public static int pangkat(int x, int y){
+        if (y == 0) {
+            return 1;
+        } else {
+            return x * pangkat(x, y - 1);
+        }
+    }
+
+    public static int pangkatIterative(int x, int y){
+        int hasil = 1;
+
+        for (int i = 1; i <= y; i++) {                      
+            hasil = hasil * x;
+            System.out.print(x + "x");
+        }
+        System.out.print(1 + "=");
+        return hasil;
+    }
+    public static void main(String[] args) {
+        System.out.println(pangkat(6, 4));
+        System.out.println(pangkatIterative(2, 5));
+    }
+}
